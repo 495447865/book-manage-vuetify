43b208575c26a6cd942850178a6d6d7397e981dc
harriszhang <harriszhang@live.cn>
2019-11-27 08:12:33 +0800

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,15 +13,27 @@
                 <book-data-table></book-data-table>
             </v-flex>
         </v-layout>
+
+        <h3>form</h3>
+        <v-layout>
+            <v-flex xs12>
+                <book-form></book-form>
+            </v-flex>
+        </v-layout>
     </v-container>
 </template>
 
 <script>
     import BookSimpleTable from './BookSimpleTable';
     import BookDataTable from './BookDataTable';
+    import BookForm from './BookForm';
 
     export default {
         name: 'HelloWorld',
-        components: { 'book-simple-table': BookSimpleTable, 'book-data-table': BookDataTable }
+        components: {
+            'book-simple-table': BookSimpleTable,
+            'book-data-table': BookDataTable,
+            'book-form': BookForm
+        }
     };
 </script>
