1d92ac021c65e8570ac6f790ba38afa54c66a607
harriszhang <harriszhang@live.cn>
2019-11-29 14:29:09 +0800

    temp save

--- src/components/BookDataTable.vue
+++ src/components/BookDataTable.vue
@@ -8,60 +8,7 @@
     export default {
         data() {
             return {
-                headers: [
-                    {
-                        "value": "name",
-                        "text": "书名"
-                    },
-                    {
-                        "value": "price",
-                        "text": "定价"
-                    },
-                    {
-                        "value": "brief",
-                        "text": "简介"
-                    },
-                    {
-                        "value": "singleTag",
-                        "text": "单选标签"
-                    },
-                    {
-                        "value": "multiTag",
-                        "text": "多选标签"
-                    },
-                    {
-                        "value": "singleStatus",
-                        "text": "单选状态"
-                    },
-                    {
-                        "value": "multiStatus",
-                        "text": "多选状态"
-                    },
-                    {
-                        "value": "singleColor",
-                        "text": "单选颜色"
-                    },
-                    {
-                        "value": "multiColor",
-                        "text": "多选颜色"
-                    },
-                    {
-                        "value": "singleCategory",
-                        "text": "单选分类"
-                    },
-                    {
-                        "value": "multiCategory",
-                        "text": "多选分类"
-                    },
-                    {
-                        "value": "singlePattern",
-                        "text": "单选形态"
-                    },
-                    {
-                        "value": "multiPattern",
-                        "text": "多选形态"
-                    }
-                ],
+                headers: [],
                 items: []
             }
         },
