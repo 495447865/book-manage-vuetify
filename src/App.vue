35a07254df1849255526345418108be2f7308ddd
harriszhang <harriszhang@live.cn>
2019-12-04 21:26:33 +0800

    add breadcrumb

--- src/App.vue
+++ src/App.vue
@@ -50,6 +50,8 @@
 
         </v-toolbar>
 
+        <v-breadcrumbs :items="items"></v-breadcrumbs>
+
         <v-content>
             <HelloWorld />
         </v-content>
@@ -68,7 +70,23 @@
         },
 
         data: () => ({
-            //
+            items: [
+                {
+                    text: 'Dashboard',
+                    disabled: false,
+                    href: 'breadcrumbs_dashboard',
+                },
+                {
+                    text: 'Link 1',
+                    disabled: false,
+                    href: 'breadcrumbs_link_1',
+                },
+                {
+                    text: 'Link 2',
+                    disabled: true,
+                    href: 'breadcrumbs_link_2',
+                },
+            ],
         }),
     };
 </script>
