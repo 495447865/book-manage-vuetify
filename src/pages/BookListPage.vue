97c821a54740c8fbcac5262ba226c86cf8f585cb
harriszhang <harriszhang@live.cn>
2019-12-11 08:27:40 +0800

    移除面包屑，后面需要找回

--- src/pages/BookListPage.vue
+++ src/pages/BookListPage.vue
@@ -1,11 +1,5 @@
 ﻿<template>
     <v-container>
-        <v-breadcrumbs :items="items"></v-breadcrumbs>
-
-        <div class="mb-12 pb-8">
-            <v-btn class="ma-2" outlined color="teal">新建</v-btn>
-        </div>
-
         <book-data-table></book-data-table>
     </v-container>
 </template>
@@ -17,17 +11,5 @@
         components: {
             'book-data-table': BookDataTable,
         },
-        data: () => ({
-            items: [
-                {
-                    text: '首页',
-                    to: { name: 'welcome' },
-                },
-                {
-                    text: '图书列表页',
-                    to: { name: 'book-list-page' },
-                },
-            ],
-        }),
     };
 </script>
