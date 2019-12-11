f7318df40070a6c5cf642364580493a78c1f3e35
harriszhang <harriszhang@live.cn>
2019-12-11 08:15:41 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,16 @@
-﻿<template>
+<template>
     <div>
         <main-header></main-header>
-
-        <!--<main-toolbar></main-toolbar>-->
-
         <router-view />
-
-        <main-footer></main-footer>
     </div>
 </template>
 
-
 <script>
     import MainHeader from './components/MainHeader';
-    import MainFooter from './components/MainFooter';
-    //import MainToolbar from './components/MainToolbar';
 
     export default {
-
         components: {
             'main-header': MainHeader,
-            'main-footer': MainFooter,
-            //'main-toolbar': MainToolbar
         },
     };
 </script>
